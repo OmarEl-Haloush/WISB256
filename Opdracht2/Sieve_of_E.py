53306cab47ea33d87fda6abff166f9934f7865b7
omarel_haloush <m.o.el-haloush@students.uu.nl>
2015-04-30 13:04:17 +0000

    Opdracht 2 reading command line for N and writing/creating file

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,8 +1,11 @@
 import time
-
-#N=input('Compute all Prime numbers <N\nN= ')
-N=100000
+import sys
+# sys.argv is a list with the command-line arguments. sysv.arg[0] is the name of Python script
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
 T1=time.perf_counter()
+N=sys.argv[1]
+file=sys.argv[2]
 n=int(N)
 string=[]
 x=2
@@ -14,16 +17,19 @@ for i in string:
     while i in string:
         if i*j<=n:
             p=i*j
+            
             if p in string:
                 string.remove(p)
+                
             j+=1
         else:
             i+=1
-fout=open('prime.dat','w')
+fout=open(sys.argv[2],'w')
 i=0
 for i in string:
     fout.write(str(i))
     fout.write('\n')
+ 
 fout.close()
 T2=time.perf_counter()
-print('Found',len(string),'Prime numbers smaller than',N,'in',T2-T1,'sec.')
+print('Time required to find',len(string),'in',T2-T1,'sec.')
