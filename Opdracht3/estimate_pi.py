ebedab1c718ac43450e22bf4d4620d9d8e87f2c0
omarel_haloush <m.o.el-haloush@students.uu.nl>
2015-05-21 14:36:27 +0000

    ??

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -4,43 +4,45 @@ import sys
 # sys.argv is a list with the command-line arguments. sysv.arg[0] is the name of Python script
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
-N=int(sys.argv[1])
-L=int(sys.argv[2])
-Seed=int(sys.argv[3])
-h=0
-p=False
-if len(sys.argv) <=3:
-    print('Use: estimate_pi.py N L Seed')
-elif L>1:
+if len(sys.argv) <3:
+    print('Use: estimate_pi.py N L')
+elif float(sys.argv[2])>1:
     print('AssertionError: L should be smaller than 1')
 else:
+    N=int(sys.argv[1])
+    L=float(sys.argv[2])
+    h=0
     def dropneedle(L):
-        p=False
-        #random.seed(Seed)
         x=random.random()
         y=random.random()
         a=random.vonmisesvariate(0,0)
-        print(x,y,a)
         endpoint1=x+L*math.cos(a)
         endpoint2=y+L*math.sin(a)
-        print(endpoint1,endpoint2)
-        if endpoint1>=0:
-            return False 
-        elif  endpoint2<=0:
-            return False
-        else:
+        if endpoint1<=((2*L)/math.pi):
             return True
-    while N>0:
-        if dropneedle(L)==True:
-            h+=1
-            print(h,N)
-            N-=1
+        elif  endpoint2>=((2*L)/math.pi):
+            return True
         else:
-            N-=1
-    print(p,N,h)        
+            return False
+    while N>0:
+        try:
+            if float(sys.argv[3])>=0:
+                random.seed(sys.argv[3])
+                if dropneedle(L)==True:
+                    h+=1
+                    N-=1
+                else:
+                    N-=1
+        except:
+            if dropneedle(L)==True:
+                h+=1
+                N-=1
+            else:
+                N-=1
     if N==0 and h>0:
-        Pi=int(sys.argv[1])/h
+        Pi=(2*L*int(sys.argv[1]))/h
         print(h,' hits in ', sys.argv[1], 'Tries')
         print('Pi= ', Pi)
+        
         
         
