ecf8f39319fc1ceb35ea4cc75d42d914c7e870aa
omarel_haloush <m.o.el-haloush@students.uu.nl>
2015-05-26 10:32:42 +0000

    roots!

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -4,11 +4,10 @@ import math
   #  return f
 def findRoot(f,a,b,epsilon):
     
-    m=int((a+b)/2)
+    m=(a+b)/2
     b=float(b)
     a=float(a)
     epsilon=float(epsilon)
-    print(f(a),f(b))
     #if int((a+b)/2)
     #if f(a)>0 and f(b)<0:
      #   print('No root found')
@@ -16,19 +15,23 @@ def findRoot(f,a,b,epsilon):
     if (b-a)<=epsilon:
         print('Error')
         return m
-    elif f(a)<0.0:
-        findRoot(f,a,m,epsilon)
-    elif f(b)<0.0: 
-        findRoot(f,m,b,epsilon)
-    elif f(a)==0:
-        print('Found root=',a)
+    elif abs(f(a))<(epsilon*2):
+        print('Found root=',"{:.2f}".format(a))
         return a
-    elif f(b)==0:
-        print('Found root=',b)
+    elif abs(f(b))<(epsilon*2):
+        print('Found root=',"{:.2f}".format(b))
         return b
+    elif f(a)<0.0 and f(m)>0.0:
+        return findRoot(f,a,m,epsilon)
+    elif f(b)<0.0 and f(m)>0.0: 
+        return findRoot(f,m,b,epsilon)
+    elif f(a)>0.0 and f(m)<0.0:
+        return findRoot(f,a,m,epsilon)
+    elif f(b)>0.0 and f(m)<0.0: 
+        return findRoot(f,m,b,epsilon)    
     else:
         print('No root found(2)')
 
-#root=findRoot(lambda x:x-1,0,3,0.1) 
-#print(root)
-#print(findRoot(lambda x:x-1,0,3,0.1))
+'''root=findRoot(lambda x:x-1,0,10,0.1) 
+print(root)
+print(findRoot(lambda x:x-1,0,10,0.1))'''
