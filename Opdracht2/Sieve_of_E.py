fec7eba76a364a5fad4a3486e7a3ff878c737c85
omarel_haloush <m.o.el-haloush@students.uu.nl>
2015-06-09 14:38:38 +0000

    Fast

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,44 +1,30 @@
 import time
 import sys
 import math
-# sys.argv is a list with the command-line arguments. sysv.arg[0] is the name of Python script
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
+# initialize the 
 T1=time.perf_counter()
-N=sys.argv[1]
-file=sys.argv[2]
-n=int(N)
-string=list(range(2,int(N)+1))
-x=2
-i=2
-j=i 
-string
-while i <=math.sqrt(n):
-    #if i in string: 
-    if i>=10 and j>=1000:
-        i+=1
-        j=i
-    if i*j in string:
-        p=i*j
-        string.remove(p)
-        #if p-1>0 and p-1<len(string):
-         #   fout=open(sys.argv[2],'w')
-          #  fout.write(str(string[p-1])+'\n')
-            #fout.write('\n')
-           # fout.close()
-        j+=1    
-    elif i*j<=n:
-        j+=1
-    else:
-        i+=1
-        j=i
+N=int(sys.argv[1])
+PrimeList= list(range(0,N))
+PrimeList[1] =0
+# loop
+for i in range(1,int(math.sqrt(N))):
+    if PrimeList[i] !=0:
+        for j in range(i*i,N,i):
+            PrimeList[j]=0
+PrimeList1=list()   
+fout=open(sys.argv[2],'w')
+for z in PrimeList:
+    if z>0:
+        PrimeList1.append(z)
+        fout.write(str(z))
+        fout.write('\n')
         
-#fout=open(sys.argv[2],'w')
-#i=0
-#for i in string:
- #   fout.write(str(i))
- #   fout.write('\n')
- 
+'''while PrimeList[z]<(len(PrimeList)-1):
+    print(PrimeList[z])
+    print(PrimeList[z],len(PrimeList),z)
+    if PrimeList[z]==0:
+        PrimeList.remove(0)
+    z+=1    '''
 
-T2=time.perf_counter()
-print('Time required to find',len(string),'in',T2-T1,'sec.')
+T2=time.perf_counter()        
+print('Time required to find',len(PrimeList1),'in',T2-T1,'sec.')
