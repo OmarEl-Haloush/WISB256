0d6614968ffd2d651bc02a410fa7bafc77298a94
omarel_haloush <m.o.el-haloush@students.uu.nl>
2015-06-16 14:22:20 +0000

    asbjd

--- Opdracht6/lorenz.py
+++ Opdracht6/lorenz.py
@@ -8,46 +8,32 @@ import numpy
 class Lorenz:
     #def __slots__(self):
      #   ('_initial','_sigma','_rho','_beta')
-    def __init__(self,initial,sigma=10,rho=10,beta=10):
-        self.initial=initial
+    def __init__(self,initial,sigma=10,rho=28,beta=8/3):
         self.sigma=sigma
         self.rho=rho
         self.beta=beta
         self.xyz=initial
-        self.x=self.initial[0]
-        self.y=self.initial[1]
-        self.z=self.initial[2]
-        x=initial[0]
-        y=initial[1]
-        z=initial[2]
-        self.funcy=(x*(rho-z-y))
-        self.funcx=(sigma*(y-x))
-        self.funcz=((x*y)-beta*z)
         return
     def __str__(self):
         new1=str()
-        for i in self.initial:
+        for i in self.xyz:
             new=i
             new1+=str(new)
             new1+=(str(' '))
         return str((new1)+'\n'+"{:.6f}".format(self.sigma)+'\n'+"{:.6f}".format(self.rho)+'\n'+"{:.6f}".format(self.beta)+'\n')
     def function(self,xyz,t):
         q=[0,0,0]
-        
-        xinit=[0,0]
-        yinit=[0,0]
-        zinit=[0,0]
-        
-        q[1]=(xyz[0]*(self.rho-xyz[2]-xyz[1]))
         q[0]=(self.sigma*(xyz[1]-xyz[0]))
+        q[1]=(xyz[0]*(self.rho-xyz[2]-xyz[1]))
         q[2]=((xyz[0]*xyz[1])-self.beta*xyz[2])
-       
         #print(x1_0,y1_0,z1_0)
         #return x1_0,y1_0,z1_0
-        return q    
-    def solve(self,T=100,dt=0.02):
-        Tmax=numpy.arange(0,T,dt)
+        xyz=q
+        return q
         
-        opl=odeint(self.function,self.xyz,Tmax)
+    def solve(self,T,dt):
+        Tmax=numpy.arange(0,T,dt)
+        opl1=list
+        opl=(odeint(self.function,self.xyz,Tmax))
         
         return opl
